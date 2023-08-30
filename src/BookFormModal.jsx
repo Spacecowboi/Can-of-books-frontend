58c4c14f7cb4e5c85c03c44803cda87b5c867576
Spacecowboi <cunningham2192@gmail.com>
2023-08-30 15:56:59 -0700

    included modal functionality to edit books via PUT request to backend, just need current backend code to be completed

--- src/BookFormModal.jsx
+++ src/BookFormModal.jsx
@@ -2,67 +2,88 @@ import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import React from 'react';
 import Form from 'react-bootstrap/Form';
+const url=import.meta.env.VITE_server_url
 
-class BookFormModal extends React.Component{
-    handleSubmit = (event) => {
-        event.preventDefault();
-        const formTitle = event.target.elements.formTitle.value;
-        const formDescription = event.target.elements.formDescription.value;
-        const formStatus = event.target.elements.formStatus.value;
 
-        this.props.onModalSave(formTitle, formDescription, formStatus);
-        this.props.onModalClose();
+class BookFormModal extends React.Component {
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const formTitle = event.target.elements.formTitle.value;
+    const formDescription = event.target.elements.formDescription.value;
+    const formStatus = event.target.elements.formStatus.value;
+
+    if (this.props.editingBook) {
+      // If editing, send our updated data to the server and handle the response
+      const updatedData = { title: formTitle, description: formDescription, status: formStatus };
+      try {
+        const response = await fetch(`${url}/books/${this.props.editingBook._id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(updatedData),
+        });
+
+        if (response.ok) {
+          // Handle success, update the book list or refresh data as needed
+          this.props.onModalClose();
+        } else {
+          // handle whatever error pops up
+        }
+      } catch (error) {
+        // handle whatever error pops up
+      }
+    } else {
+      this.props.onModalSave(formTitle, formDescription, formStatus);
+      this.props.onModalClose();
     }
+  }
 
-    
-render(){
+  render() {
+    const { showNewBookForm, editingBook } = this.props;
+    console.log(this.props)
     return (
-        <Modal
-        show={this.props.showNewBookForm}
-        >
-            <Modal.Header closeButton>
-              <Modal.Title>Add Book</Modal.Title>
-            </Modal.Header>
+      <Modal show={showNewBookForm}>
+        <Modal.Header closeButton>
+          <Modal.Title>{editingBook ? 'Edit Book' : 'Add Book'}</Modal.Title>
+        </Modal.Header>
         
-            <Modal.Body>
-            <form onSubmit={this.handleSubmit}>
-                <Form.Group className="mb-3" controlId="formTitle">
-                    <Form.Label>Title</Form.Label>
-                    <Form.Control required type="title" placeholder="Enter title" />
-                </Form.Group>
+        <Modal.Body>
+          <form onSubmit={this.handleSubmit}>
+            <Form.Group className="mb-3" controlId="formTitle">
+              <Form.Label>Title</Form.Label>
+              <Form.Control required type="text" defaultValue={editingBook ? editingBook.title : ''} />
+            </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formDescription">
-                    <Form.Label>Description</Form.Label>
-                    <Form.Control  required type="textarea" placeholder="Enter description" />
-                </Form.Group>
+            <Form.Group className="mb-3" controlId="formDescription">
+              <Form.Label>Description</Form.Label>
+              <Form.Control required as="textarea" defaultValue={editingBook ? editingBook.description : ''} />
+            </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formStatus">
-                    <Form.Label>Status</Form.Label>
-                    <Form.Select  required type="select" placeholder="Enter status">
-                    <option value="Available">Available</option>
-                    <option value="Checked Out">Checked Out</option>
-                    <option value="On Hold">On Hold</option>
-                </Form.Select>
-                </Form.Group>
-                <Button type='submit' variant="dark">Save Book</Button>
-              </form>
-            </Modal.Body>
+            <Form.Group className="mb-3" controlId="formStatus">
+              <Form.Label>Status</Form.Label>
+              <Form.Select required defaultValue={editingBook ? editingBook.status : 'Available'}>
+                <option value="Available">Available</option>
+                <option value="Checked Out">Checked Out</option>
+                <option value="On Hold">On Hold</option>
+              </Form.Select>
+            </Form.Group>
+
+            <Button type='submit' variant="dark">
+              {editingBook ? 'Save Changes' : 'Save Book'}
+            </Button>
+          </form>
+        </Modal.Body>
     
-            <Modal.Footer>
-              <Button onClick={this.props.onModalClose} variant="secondary">Close Without Saving</Button>
-
-            </Modal.Footer>
-        </Modal>
-      );
-
-
-    }
-
-
-
+        <Modal.Footer>
+          <Button onClick={this.props.onModalClose} variant="secondary">
+            Close Without {editingBook ? 'Saving Changes' : 'Saving'}
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
 }
-
-
 
 
 
